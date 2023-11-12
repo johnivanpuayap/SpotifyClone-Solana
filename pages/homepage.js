4995cb6a8f7d80379d9a540695b7bd4a26d32aa6
johnivanpuayap <johnivanpuayap@gmail.com>
2023-11-12 19:29:46 +0800

    Add Header

--- pages/homepage.js
+++ pages/homepage.js
@@ -1,11 +1,23 @@
 import Nav from '../components/nav'
 import Activity from '../components/activity'
+import { useEffect, useState } from 'react'
+import Header from '../components/Header'
 
 const HomePage = () => {
+
+  const [showUploadMusic, setUploadMusic] = useState(false)
+  const [title, setTitle] = useState('')
+  const [musicUrl, setMusicUrl] = useState('')
+  const [songs, setSongs] = useState([])  
+
   return (
     <div className='flex'>
       <Nav />
-      <div className='w-full'>{/* Start coding here */}</div>
+      <div className='w-full'>
+        <Header/>
+        {/* <Playlist /> */}
+        {/* <PlayerControls /> */}
+      </div>
       <Activity />
     </div>
   )
