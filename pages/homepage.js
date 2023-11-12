cea82657dbcffe2997929436fa534fce19708315
johnivanpuayap <johnivanpuayap@gmail.com>
2023-11-12 20:21:22 +0800

    Add Playlist component

--- pages/homepage.js
+++ pages/homepage.js
@@ -2,6 +2,10 @@ import Nav from '../components/nav'
 import Activity from '../components/activity'
 import { useEffect, useState } from 'react'
 import Header from '../components/Header'
+import uploadModal from '../components/uploadModal'
+import playlist from '../components/playlist'
+
+import useSpotify from '../hooks/useSpotify'
 
 const HomePage = () => {
 
@@ -10,13 +14,31 @@ const HomePage = () => {
   const [musicUrl, setMusicUrl] = useState('')
   const [songs, setSongs] = useState([])  
 
+  const { newMusic, getSongs} = useSpotify(
+    musicUrl,
+    title,
+    setTitle,
+    setMusicUrl,
+    setShowUploadMusic,
+  )
+
   return (
     <div className='flex'>
       <Nav />
       <div className='w-full'>
         <Header setShowUploadMusic = {setShowUploadMusic}/>
-        {/* <Playlist /> */}
+        <Playlist/>
         {/* <PlayerControls /> */}
+        {showUploadMusic && (
+          <uploadModal
+            title={title}
+            setTitle={setTitle}
+            setShowUploadMusic={setShowUploadMusic}
+            musicUrl={musicUrl}
+            setMusicUrl={setMusicUrl}
+            newMusic={newMusic}
+          />
+        )}
       </div>
       <Activity />
     </div>
